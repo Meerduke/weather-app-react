66ed90913f0b57e4936bb2ead0743a5db81edef3
Meerduke <Meerduke@users.noreply.github.com>
2021-03-08 14:25:00 +0100

    Icon colors changed

--- src/WeatherIcons.js
+++ src/WeatherIcons.js
@@ -3,30 +3,30 @@ import ReactAnimatedWeather from 'react-animated-weather';
 
 export default function WeatherIcons(props){ 
     const codeMapping = {
-        "01d": "CLEAR_DAY" ,
-        "01n": "CLEAR_NIGHT",
-        "02d": "PARTLY_CLOUDY_DAY",
-        "02n": "PARTLY_CLOUDY_NIGHT",
-        "03d": "CLOUDY",
-        "03n": "CLOUDY",
-        "04d": "PARTLY_CLOUDY_DAY",
-        "04n": "PARTLY_CLOUDY_NIGHT",
-        "09d": "RAIN",
-        "09n": "RAIN",
-        "10d": "RAIN",
-        "10n": "RAIN",
-        "11d": "RAIN",
-        "11n": "RAIN",
-        "13d": "SNOW",
-        "13n": "SNOW",
-        "50d": "FOG",
-        "50n": "FOG"
+        "01d": {icon: "CLEAR_DAY", color: "#F79954"},
+        "01n": {icon: "CLEAR_NIGHT", color: "#63676A"},
+        "02d": {icon: "PARTLY_CLOUDY_DAY", color: "#F79954"},
+        "02n": {icon: "PARTLY_CLOUDY_NIGHT", color: "#63676A"},
+        "03d": {icon: "CLOUDY", color: "#63676A"},
+        "03n": {icon: "CLOUDY", color: "#63676A"},
+        "04d": {icon: "PARTLY_CLOUDY_DAY", color: "#F79954"},
+        "04n": {icon: "PARTLY_CLOUDY_NIGHT", color: "#63676A"},
+        "09d": {icon: "RAIN", color: "#1777C5"},
+        "09n": {icon: "RAIN", color: "#1777C5"},
+        "10d": {icon: "RAIN", color: "#1777C5"},
+        "10n": {icon: "RAIN", color: "#1777C5"},
+        "11d": {icon: "RAIN", color: "#1777C5"},
+        "11n": {icon: "RAIN", color: "#1777C5"},
+        "13d": {icon: "SNOW", color: "#A8C2E3"},
+        "13n": {icon: "SNOW", color: "#A8C2E3"},
+        "50d": {icon: "FOG", color: "#D2D2D2"},
+        "50n": {icon: "FOG", color: "#D2D2D2"}
     }
     return(
         <div>
             <ReactAnimatedWeather
-                icon={codeMapping[props.code]}
-                color="#0f3057"
+                icon={codeMapping[props.code].icon}
+                color={codeMapping[props.code].color}
                 size={60}
                 animate= {false}
             />
